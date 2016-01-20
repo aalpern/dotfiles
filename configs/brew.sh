b093c2a51cf3ca6069dd670935253709f9b0849a
Adam Alpern <adam@anki.com>
2016-01-20 15:31:01 -0800

    Add syntax highlighting in less

---
+++ configs/brew.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+if [[ -z `which brew` ]]; then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+brew install go nvm source-highlight
