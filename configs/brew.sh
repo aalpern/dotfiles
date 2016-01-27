279182c7eccf9a4c1bbf3cdc0b7d4e97b961e17d
Adam Alpern <adam.alpern@gmail.com>
2016-01-26 21:31:59 -0800

    Add tree

--- configs/brew.sh
+++ configs/brew.sh
@@ -4,4 +4,4 @@ if [[ -z `which brew` ]]; then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-brew install go nvm source-highlight
+brew install go nvm source-highlight tree
