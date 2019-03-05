74eaba05cbcd877f4d98ed8a4e90cdc9fee1c326
Adam Alpern <adam@anki.com>
2019-03-05 11:40:37 -0800

    Set tab indent size for Go

--- home/.emacs.d/init/init-go.el
+++ home/.emacs.d/init/init-go.el
@@ -1,7 +1,8 @@
 (defun aa-go-mode-hook ()
   (remove-hook 'font-lock-mode-hook 'hc-highlight-tabs)
   ;; (hc-toggle-highlight-tabs)
-  (setq indent-tabs-mode t))
+  (setq indent-tabs-mode t)
+  (setq tab-width 4))
 
 (add-hook 'go-mode-hook 'aa-go-mode-hook)
 (add-hook 'before-save-hook 'gofmt-before-save)
